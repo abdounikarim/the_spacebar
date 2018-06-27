4af74bbbd57e57061999a89f83d3dda6ba474e6b
Karim <abdounikarim@gmail.com>
2018-06-27 10:01:56 +0200

    updating an entity with new fields

--- src/Controller/ArticleAdminController.php
+++ src/Controller/ArticleAdminController.php
@@ -38,6 +38,11 @@ EOF
         if (rand(1, 10) > 2) {
             $article->setPublishedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
         }
+
+        $article->setAuthor('Mike Ferengi')
+            ->setHeartCount(rand(5,100))
+            ->setImageFilename('asteroid.jpeg');
+
         $em->persist($article);
         $em->flush();
         return new Response(sprintf(
