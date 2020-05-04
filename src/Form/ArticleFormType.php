132be762f77f12f51aeac70fa8235741b2dc515d
abdounikarim <abdounikarim@gmail.com>
2020-05-04 12:15:53 +0200

    Form Type Extension

--- src/Form/ArticleFormType.php
+++ src/Form/ArticleFormType.php
@@ -27,19 +27,13 @@ class ArticleFormType extends AbstractType
             ->add('title', TextType::class, [
                 'help' => 'Choose something catchy!'
             ])
-            ->add('content')
+            ->add('content', null, [
+                'rows' => 15
+            ])
             ->add('publishedAt', null, [
                 'widget' => 'single_text'
             ])
-            ->add('author', EntityType::class, [
-                'class' => User::class,
-                'choice_label' => function(User $user) {
-                    return sprintf('(%d) %s', $user->getId(), $user->getEmail());
-                },
-                'placeholder' => 'Choose an author',
-                'choices' => $this->userRepository->findAllEmailAlphabetical(),
-                'invalid_message' => 'Symfony is too smart for your hacking!'
-            ])
+            ->add('author', UserSelectTextType::class)
         ;
     }
 
