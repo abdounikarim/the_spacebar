c8edff48ec3c55ab4c82b6045901cbcd2ba07758
abdounikarim <abdounikarim@gmail.com>
2020-05-04 15:45:00 +0200

    URL to Public Assets

--- src/Service/UploaderHelper.php
+++ src/Service/UploaderHelper.php
@@ -7,6 +7,8 @@ use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class UploaderHelper
 {
+    const ARTICLE_IMAGE = 'article_image';
+
     private $uploadsPath;
 
     public function __construct(string $uploadsPath)
@@ -16,7 +18,7 @@ class UploaderHelper
 
     public function uploadArticleImage(UploadedFile $uploadedFile): string
     {
-        $destination = $this->uploadsPath.'/article_image';
+        $destination = $this->uploadsPath.'/'.self::ARTICLE_IMAGE;
 
         $originalFilename = pathinfo($uploadedFile->getClientOriginalName(), PATHINFO_FILENAME);
         $newFilename = Urlizer::urlize($originalFilename).'-'.uniqid().'.'.$uploadedFile->guessExtension();
@@ -27,5 +29,10 @@ class UploaderHelper
         );
 
         return $newFilename;
+    }
+
+    public function getPublicPath(string $path): string
+    {
+        return 'uploads/'.$path;
     }
 }
--- src/Twig/AppExtension.php
+++ src/Twig/AppExtension.php
@@ -3,25 +3,27 @@
 namespace App\Twig;
 
 use App\Service\MarkdownHelper;
+use App\Service\UploaderHelper;
 use Psr\Container\ContainerInterface;
-use Symfony\Component\DependencyInjection\ServiceSubscriberInterface;
+use Symfony\Contracts\Service\ServiceSubscriberInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 class AppExtension extends AbstractExtension implements ServiceSubscriberInterface
 {
-    /**
-     * @var MarkdownHelper
-     */
     private $container;
 
-    /**
-     * AppExtension constructor.
-     */
     public function __construct(ContainerInterface $container)
     {
         $this->container = $container;
+    }
+
+    public function getFunctions(): array
+    {
+        return [
+            new TwigFunction('uploaded_asset', [$this, 'getUploadedAssetPath'])
+        ];
     }
 
     public function getFilters(): array
@@ -38,10 +40,18 @@ class AppExtension extends AbstractExtension implements ServiceSubscriberInterfa
             ->parse($value);
     }
 
+    public function getUploadedAssetPath(string $path): string
+    {
+        return $this->container
+            ->get(UploaderHelper::class)
+            ->getPublicPath($path);
+    }
+
     public static function getSubscribedServices()
     {
         return [
-            MarkdownHelper::class
+            MarkdownHelper::class,
+            UploaderHelper::class,
         ];
     }
 }
