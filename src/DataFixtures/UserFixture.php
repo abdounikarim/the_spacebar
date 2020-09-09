9e564a02a3a009f521cb0a2a4ed88bfdca90423b
abdounikarim <abdounikarim@gmail.com>
2020-09-09 11:35:03 +0200

    File Uploads & Data Fixtures

--- src/DataFixtures/UserFixture.php
+++ src/DataFixtures/UserFixture.php
@@ -22,7 +22,7 @@ class UserFixture extends BaseFixtures
             $user = new User();
             $user->setEmail(sprintf('spacebar%d@example.com', $i));
             $user->setFirstName($this->faker->firstName);
-            $user->agreeTerms();
+            $user->agreeToTerms();
 
             if ($this->faker->boolean) {
                 $user->setTwitterUsername($this->faker->userName);
@@ -46,7 +46,7 @@ class UserFixture extends BaseFixtures
             $user->setEmail(sprintf('admin%d@thespacebar.com', $i));
             $user->setFirstName($this->faker->firstName);
             $user->setRoles(['ROLE_ADMIN']);
-            $user->agreeTerms();
+            $user->agreeToTerms();
 
             $user->setPassword($this->passwordEncoder->encodePassword(
                 $user,
