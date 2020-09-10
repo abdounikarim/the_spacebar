5c5904b7197ddf3b83a636f130dd184e603a0184
abdounikarim <abdounikarim@gmail.com>
2020-09-10 15:20:18 +0200

    Dropzone: AJAX Upload

--- public/js/admin_article_form.js
+++ public/js/admin_article_form.js
@@ -1,4 +1,8 @@
+Dropzone.autoDiscover = false;
+
 $(document).ready(function() {
+    initializeDropzone();
+
     var $locationSelect = $('.js-article-form-location');
     var $specificLocationTarget = $('.js-specific-location-target');
 
@@ -24,3 +28,14 @@ $(document).ready(function() {
         });
     });
 });
+
+function initializeDropzone() {
+    var formElement = document.querySelector('.js-reference-dropzone');
+    if (!formElement) {
+        return;
+    }
+
+    var dropzone = new Dropzone(formElement, {
+        paramName: 'reference'
+    });
+}
